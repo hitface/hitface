fe7a24ea44817524c60adde60044f5e703201cf1
unknown <unknowngid000@gmail.com>
2019-06-11 23:34:06 +0700

    init

--- zalo.api.js
+++ zalo.api.js
@@ -2,22 +2,33 @@ Zalo.init({
     version: '2.0',
     appId: '3998092944035386293',
     redirectUrl: 'https://hitface.github.io/'
-  }
+}
 );
 
-Zalo.getLoginStatus(function(response) {
-    if (response.status === 'connected') {
-      Zalo.api('/me',
-        'GET',
-        {
-          fields: 'id,name'
-        },
-        function (response) {
-          console.log(response);
+function getToken() {
+    Zalo.getLoginStatus(function (response) {
+        if (response.status === 'connected') {
+            Zalo.api('/me',
+                'GET',
+                {
+                    fields: 'id,name'
+                },
+                function (response) {
+                    console.log(response);
+                }
+            );
+        } else {
+            Zalo.login();
         }
-      );
-    } else {
-       	
-Zalo.login();
+    });
+
+    const Http = new XMLHttpRequest();
+    const url = 'https://oauth.zaloapp.com/v3/auth?app_id=3998092944035386293&redirect_uri=https://hitface.github.io/';
+    Http.open("GET",url);
+    Http.send();
+
+    Http.onreadystatechange=(e)=>{
+        console.log(Http.response);
     }
-  });
+
+}
