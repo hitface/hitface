cdb4544afcecdcc4ff4fc8e6c4a8f421accfd1f5
unknown <unknowngid000@gmail.com>
2019-06-11 23:16:55 +0700

    update code

---
+++ zalo.api.js
@@ -0,0 +1,22 @@
+Zalo.init({
+    version: '2.0',
+    appId: '3998092944035386293',
+    redirectUrl: 'https://hitface.github.io/'
+  }
+);
+
+Zalo.getLoginStatus(function(response) {
+    if (response.status === 'connected') {
+      Zalo.api('/me',
+        'GET',
+        {
+          fields: 'id,name'
+        },
+        function (response) {
+          console.log(response);
+        }
+      );
+    } else {
+      Zalo.login(curentState);
+    }
+  });
